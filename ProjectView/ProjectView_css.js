f2068f2111c48907fb95a204737a7389dd5cd8d4
nipasarker01 <nipasarker.hd1@gmail.com>
2017-11-10 19:50:25 +0530

    Update ProjectView_css.js

--- ProjectView/ProjectView_css.js
+++ ProjectView/ProjectView_css.js
@@ -1,79 +1,120 @@
+import React, { Component } from 'react';
 import {
-    StyleSheet,
-} from 'react-native';
+  Text,
+  Image,
+  View,
+  ScrollView,
+  StatusBar,
+  TouchableOpacity ,
+ } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Left, Body, Right, Button, Footer} from 'native-base';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-module.exports = StyleSheet.create({
-  container: {
-     flex: 1,
-     marginTop: 100,
-     alignItems: 'center',
-   },
-   cardHeading: {
-     fontSize: 15,
-
-   },
-   cardImage: {
-     height: 200,
-     flex:1,
-
-   },
-   iconContainer:{
-
-     flexDirection: 'row',
-     alignItems: 'center',
-     paddingVertical:20,
-     paddingHorizontal:100,
-   },
-   iconStyle: {
-
-     color:'red',
-     justifyContent:'center',
-   },
-
-   iconStylebtm:{
-     color:'rgb(230,176,178)',
-   },
-   iconText:{
-
-  paddingRight:15,
-  justifyContent:'center',
-   },
-   iconImage:{
-    position:'absolute',
-    top:0,
-    right:0,
-    backgroundColor:'transparent'
+import styles from './ProjectView_css';
 
 
-   },
-  btnOne:{
 
-   marginRight:20,
-    justifyContent:'center',
-    backgroundColor:'transparent',
-    borderRadius: 15,
-    borderWidth: .5,
-    borderColor: '#000'
-  },
-  btnTxt:{
-    paddingHorizontal:20,
-    paddingVertical:4,
+const Greenjet = require('./assets/images/projectimg.png');
 
-  },
-  btnTxt2:{
-    paddingHorizontal:20,
-    paddingVertical:4,
-    color:'#fff',
+export default class ProjectView extends Component {
 
-  },
-  btnTwo:{
+  static navigationOptions = {
+    header: null,
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: 'PROJECT DESCRIPTION', img: { Greenjet } },
 
-      justifyContent:'center',
-      backgroundColor:'red',
-      borderRadius: 15,
-      borderWidth: .5,
-      borderColor: 'red',
-      overflow:'hidden',
-  },
+      ],
+    };
+  }
 
-});
+
+
+
+  render() {
+    return (
+      <View style={{flex:1}}>
+
+      <Header />
+       <Content>
+        <View style={styles.iconContainer}>
+
+            <Icon name="favorite-border" size={25} style={styles.iconStyle} />
+            <Text style={styles.iconText}>35</Text>
+            <Icon name="share" size={25} style={styles.iconStyle} />
+              <Text style={styles.iconText}>24</Text>
+            <Icon name="bookmark" size={25} style={styles.iconStyle} />
+                <Text style={styles.iconText}>4</Text>
+                <Icon name="chat-bubble-outline" size={25} style={styles.iconStyle} />
+
+                <Text style={styles.iconText}>9</Text>
+
+        </View>
+
+        <View style={{ flexDirection: 'row',justifyContent:'center' }}>
+
+
+        <TouchableOpacity style={styles.btnOne}>
+        <Text style={styles.btnTxt} >SHARED FILES</Text>
+        </TouchableOpacity>
+
+
+             <TouchableOpacity style={styles.btnTwo}>
+             <Text style={styles.btnTxt2} >FIND COLLABS</Text>
+             </TouchableOpacity>
+
+
+       </View>
+
+
+            <Card>
+
+            <CardItem header>
+            <Left>
+
+                <Text style={styles.cardHeading}>PROJECT DESCRIPTION</Text>
+
+            </Left>
+
+            <Right>
+              <Icon name="mode-edit" size={25}  />
+
+            </Right>
+            </CardItem>
+
+              <CardItem style={{ flex:1 }}>
+                <Text style={{ textAlign:'justify'}}>
+                Green Jet Ski is a project for water adventure lovers.It is a
+                personal watercraft that can be used for sports or just to
+                have fun with your friends and familly.It is inspired on a turtle
+                and it incorporates a futuristic design that will make users
+                travel 20 years from now.
+                </Text>
+              </CardItem>
+              <CardItem cardBody>
+
+
+                <Image source={Greenjet} style={styles.cardImage} />
+                 <Icon name="mode-edit" size={25} style={styles.iconImage} />
+              </CardItem>
+
+              <CardItem style={{ flexDirection: 'row',justifyContent:'center' }} >
+
+
+                 <Icon name="fiber-manual-record" size={25} style={styles.iconStylebtm} />
+
+                 <Icon name="fiber-manual-record" size={15} style={styles.iconStylebtm} />
+
+              </CardItem>
+            </Card>
+
+</Content>
+
+<Footer />
+      </View>
+    );
+  }
+}
